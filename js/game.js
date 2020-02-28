32f91abecf5f0f0f5cce18babfb0621592346083
dwlam8790 <49571438+dwlam8790@users.noreply.github.com>
2020-02-28 08:54:36 -0500

    fixing some bugs

--- js/game.js
+++ js/game.js
@@ -1,4 +1,5 @@
-// create a new scene named "Game"
+//create a new scene
+//init()>preload()>create()>update()
 let gameScene = new Phaser.Scene('Game');
 
 // some parameters for our scene
@@ -6,13 +7,13 @@ gameScene.init = function() {}
 
 // load asset files for our game
 gameScene.preload = function() {
-//load images
+// load images
   this.load.image('background', 'assets/images/background-city.png');
   this.load.image('building', 'assets/images/building.png');
   this.load.image('car', 'assets/images/car.png');
   this.load.image('house', 'assets/images/house.png');
   this.load.image('tree', 'assets/images/tree.png');
-//load audio
+// load audio
   this.load.audio('treeAudio', 'assets/audio/arbol.mp3');
   this.load.audio('carAudio', 'assets/audio/auto.mp3');
   this.load.audio('houseAudio', 'assets/audio/casa.mp3');
@@ -20,12 +21,12 @@ gameScene.preload = function() {
   this.load.audio('correct', 'assets/audio/correct.mp3');
   this.load.audio('wrong', 'assets/audio/wrong.mp3');
 };
-};
+
 
 // executed once, after assets were loaded
 gameScene.create = function() {
   //load background
-  this.add.sprite(0, 0, 'background').setOrigin(0, 0);
+  let bg = this.add.sprite(0, 0, 'background').setOrigin(0, 0);
 
   let soundSample = this.sound.add('correct');
   soundSample.play();
